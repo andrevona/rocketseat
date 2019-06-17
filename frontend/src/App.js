157156bf40b45e0ba8ba24014ac9e0376d31657b
andrevonah <andre2889@gmail.com>
2019-06-17 17:13:02 -0300

    criado header que direciona para feed e new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 // BrowserRouter em volta dos componentes que precisam ter acesso às rotas
 
+import Header from './components/Header'
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
