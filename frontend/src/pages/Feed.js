e359521352073477cf9f33f9301f535bf85f62af
andrevonah <andre2889@gmail.com>
2019-06-17 18:39:39 -0300

    estilização do Feed a partir de dados estáticos

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render() {
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+
+                    <header>
+                        <div className="user-info">
+                            <span>André Von Ah</span>
+                            <span className="place">Indaiatuba</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/guitarra.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Minha guitarra!
+                            <span>#guitar #music #top</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+
+                    <header>
+                        <div className="user-info">
+                            <span>André Von Ah</span>
+                            <span className="place">Indaiatuba</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/guitarra.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Minha guitarra!
+                            <span>#guitar #music #top</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
