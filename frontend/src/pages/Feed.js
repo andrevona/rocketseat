0917633aa6fd7509d6003bc3a9812d824b8e3fb2
andrevonah <andre2889@gmail.com>
2019-06-18 10:01:24 -0300

    arrumando identação

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -9,51 +9,51 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
-    state = {
-        feed: [],
-    };
+   state = {
+      feed: [],
+   };
 
-    async componentDidMount() {
-        const response = await api.get('posts');
+   async componentDidMount() {
+      const response = await api.get('posts');
 
-        this.setState({ feed: response.data });
-    }
+      this.setState({ feed: response.data });
+   }
 
 
-    render() {
-        return(
-            <section id="post-list">
-                { this.state.feed.map(post => (
-                    <article key={post._id} >
-                        <header>
-                            <div className="user-info">
-                                <span>{post.author}</span>
-                                <span className="place">{post.place}</span>
-                            </div>
-                            <img src={more} alt="Mais"/>
-                        </header>
+   render() {
+      return(
+         <section id="post-list">
+               { this.state.feed.map(post => (
+                  <article key={post._id} >
+                     <header>
+                           <div className="user-info">
+                              <span>{post.author}</span>
+                              <span className="place">{post.place}</span>
+                           </div>
+                           <img src={more} alt="Mais"/>
+                     </header>
 
-                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+                     <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
 
-                        <footer>
-                            <div className="actions">
-                                <img src={like} alt=""/>
-                                <img src={comment} alt=""/>
-                                <img src={send} alt=""/>
-                            </div>
-                            
-                            <strong>{post.likes} curtidas</strong>
+                     <footer>
+                           <div className="actions">
+                              <img src={like} alt=""/>
+                              <img src={comment} alt=""/>
+                              <img src={send} alt=""/>
+                           </div>
+                           
+                           <strong>{post.likes} curtidas</strong>
 
-                            <p>
-                                {post.description}
-                                <span>{post.hashtags}</span>
-                            </p>
-                        </footer>
-                    </article>
-                )) }
-            </section>
-        );
-    }
+                           <p>
+                              {post.description}
+                              <span>{post.hashtags}</span>
+                           </p>
+                     </footer>
+                  </article>
+               )) }
+         </section>
+      );
+   }
 }
 
 export default Feed;
