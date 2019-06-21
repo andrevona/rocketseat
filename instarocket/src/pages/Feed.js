6fc444dbd5cf01a40400b901c5e268f9abcb85f0
andrevonah <andre2889@gmail.com>
2019-06-21 12:13:52 -0300

    configurando header

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
+
+import camera from '../assets/camera.png';
 
 export default class Feed extends Component {
-  render() {
-    return <View />;
-  }
+   static navigationOptions = {
+      headerRight: (
+         <TouchableOpacity style={{marginRight: 20}} onPress={() => {}} >
+            <Image source={camera} />
+         </TouchableOpacity>
+      ),
+   };
+
+   render() {
+      return <View />;
+   }
 }
