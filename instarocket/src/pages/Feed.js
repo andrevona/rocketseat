9ffadbd3c6b016f2003bf0de30f6791f9ef0a1ad
andrevonah <andre2889@gmail.com>
2019-06-21 18:31:32 -0300

    estilos definidos para o feed

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -53,19 +53,28 @@ export default class Feed extends Component {
 
                      <Image 
                         style={styles.feedImage}
-                        source={{ uri: `http://localhost:3333/${item.image}` }}
+                        source={{ uri: `http://localhost:3333/files/${item.image}` }}
                      />
 
                      <View style={styles.feedItemFooter}>
                         <View style={styles.actions}>
-                           <TouchableOpacity onPress={() => {}}>
-                              <Image source={like} />
+                           <TouchableOpacity 
+                              style={styles.action} 
+                              onPress={() => {}}>
+                                 <Image source={like} 
+                           />
                            </TouchableOpacity>
-                           <TouchableOpacity onPress={() => {}}>
-                              <Image source={comment} />
+                           <TouchableOpacity 
+                              style={styles.action} 
+                              onPress={() => {}}>
+                                 <Image source={comment} 
+                           />
                            </TouchableOpacity>
-                           <TouchableOpacity onPress={() => {}}>
-                              <Image source={send} />
+                           <TouchableOpacity 
+                              style={styles.action} 
+                              onPress={() => {}}>
+                                 <Image source={send} 
+                           />
                            </TouchableOpacity>
                         </View>
 
@@ -83,5 +92,63 @@ export default class Feed extends Component {
 }
 
 const styles = StyleSheet.create({
+   container: {
+      flex: 1
+   },
+   
+   feedItem: {
+      marginTop: 20
+   },
+
+   feedItemHeader: {
+      paddingHorizontal: 15,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+   },
+
+   name: {
+      fontSize: 14,
+      color: '#000'
+   },
+
+   place: {
+      fontSize: 12,
+      color: '#666',
+      marginTop: 2
+   },
+
+   feedImage: {
+      width: '100%',
+      height: 400,
+      marginVertical: 15
+   },
+
+   feedItemFooter: {
+      paddingHorizontal: 15
+   },
+
+   actions: {
+      flexDirection: 'row'
+   },
+
+   action: {
+      marginRight: 8
+   },
+
+   likes: {
+      marginTop: 15,
+      fontWeight: 'bold',
+      color: '#000'
+   },
+
+   description: {
+      lineHeight: 18,
+      color: '#000'
+   },
+
+   hashtags: {
+      color: '#7159c1'
+   }
 
 });
