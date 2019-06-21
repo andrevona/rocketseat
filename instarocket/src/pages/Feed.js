11f84e1fde017360adafda086bb8b16654679c3e
andrevonah <andre2889@gmail.com>
2019-06-21 16:46:55 -0300

    finalizada estrutura do Feed

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,19 +1,87 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
 import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
 
 export default class Feed extends Component {
    static navigationOptions = ({ navigation }) => ({
       headerRight: (
-         <TouchableOpacity style={{marginRight: 20}} onPress={() => navigation.navigate('New')} >
+         <TouchableOpacity 
+            style={{marginRight: 20}} 
+            onPress={() => navigation.navigate('New')} 
+         >
             <Image source={camera} />
          </TouchableOpacity>
       ),
    });
 
+   state = {
+      feed: [],
+   };
+
+   async componentDidMount() {
+      // this.registerToSocket();
+
+      const response = await api.get('posts');
+
+      this.setState({ feed: response.data });
+   }
+
    render() {
-      return <View />;
+      return (
+         <View style={styles.container}>
+            <FlatList
+               data={this.state.feed}
+               keyExtractor={post => post._id}
+               renderItem={({ item }) => (
+                  <View style={styles.feedItem}>
+                  
+                     <View style={styles.feedItemHeader}>
+                        <View style={styles.userInfo}>
+                           <Text style={styles.name}>{item.author}</Text>
+                           <Text style={styles.place}>{item.place}</Text>
+                        </View>
+
+                        <Image source={more} />
+                     </View>
+
+                     <Image 
+                        style={styles.feedImage}
+                        source={{ uri: `http://localhost:3333/${item.image}` }}
+                     />
+
+                     <View style={styles.feedItemFooter}>
+                        <View style={styles.actions}>
+                           <TouchableOpacity onPress={() => {}}>
+                              <Image source={like} />
+                           </TouchableOpacity>
+                           <TouchableOpacity onPress={() => {}}>
+                              <Image source={comment} />
+                           </TouchableOpacity>
+                           <TouchableOpacity onPress={() => {}}>
+                              <Image source={send} />
+                           </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.likes}>{item.likes} curtidas</Text>
+                        <Text style={styles.description}>{item.description}</Text>
+                        <Text style={styles.hashtags}>{item.hashtags}</Text>
+                     </View>
+
+                  </View>
+               )}
+            />
+         </View>
+      )
    }
 }
+
+const styles = StyleSheet.create({
+
+});
---
+++ instarocket/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+   baseURL: 'http://localhost:3333'
+   // baseURL: 'http://10.0.2.2:3333' // android studio emulator
+   // baseURL: 'http://10.0.3.2:3333' // genymotion android
+   // baseURL: 'http://meu_ip:3333' // via USB
+})
+
+export default api;
