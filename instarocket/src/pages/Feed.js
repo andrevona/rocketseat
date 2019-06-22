c0b1ec53210213e893abfebfab2f957d86adc4c2
andrevonah <andre2889@gmail.com>
2019-06-22 11:05:57 -0300

    realtime para novo post e curtida

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import { View, Image, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
@@ -26,11 +27,31 @@ export default class Feed extends Component {
    };
 
    async componentDidMount() {
-      // this.registerToSocket();
+      this.registerToSocket();
 
       const response = await api.get('posts');
 
       this.setState({ feed: response.data });
+   }
+
+   registerToSocket = () => {
+      const socket = io('http://localhost:3333');
+
+      socket.on('post', newPost => {
+         this.setState({ feed: [newPost, ... this.state.feed] });
+      })
+
+      socket.on('like', likedPost => {
+         this.setState({ 
+            feed: this.state.feed.map(post => 
+               post._id === likedPost._id ? likedPost : post
+            )
+          });
+      })
+   }
+
+   handleLike = id => {
+      api.post(`/posts/${id}/like`);
    }
 
    render() {
@@ -60,7 +81,7 @@ export default class Feed extends Component {
                         <View style={styles.actions}>
                            <TouchableOpacity 
                               style={styles.action} 
-                              onPress={() => {}}>
+                              onPress={() => this.handleLike(item._id)}>
                                  <Image source={like} 
                            />
                            </TouchableOpacity>
